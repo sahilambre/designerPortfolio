c29a046d9fac2dd74d81241b59045d6efcd4303c
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-08-02 01:23:05 -0400

    Marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Marquee = () => {
-  return <div className="w-full h-[50vh] bg-green-600"></div>;
+  return (
+    <div className="w-full py-20 bg-yellow-300 rounded-tl-3xl rounded-tr-3xl">
+      <div className="text border-t-2 border-b-2 border-yellow-500 flex gap-10 overflow-hidden whitespace-nowrap">
+        <h1 className="text-[16vw] leading-none font-['Helvetica'] uppercase pt-10-mb-10 font-semibold">
+          I code.
+        </h1>
+        <h1 className="text-[15vw] leading-none font-['Helvetica'] uppercase pt-10-mb-10 font-semibold">
+          I Code.
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Marquee;
